493a346df6b388c76e1f124b9adf822eea81055e
Marco Giulietti <ga6t00n@gmail.com>
2020-11-06 16:59:21 +0100

    Bonus - Random Color Title
    Creata una function per randomizzare il colore del titolo

--- js/main.js
+++ js/main.js
@@ -12,6 +12,23 @@ const app = new Vue ({
     el: '#app',
     data: {
         title: 'Benvenuti nel mondo di Vue!',
-        img: 'https://www.fillmurray.com/800/600'
+        img: 'https://www.fillmurray.com/400/300',
+        colorize: ''
+
+    },
+    methods: {
+        colorChange: function () {
+            let rand = Math.floor( Math.random() * 20) + 1
+            if (rand < 5) {
+                this.colorize = 'tomato';
+            } else if (rand >= 5 && rand < 10) {
+                this.colorize = 'crimson';
+            } else if (rand >= 10 && rand < 15) {
+                this.colorize = 'dodgerblue';
+            } else {
+                this.colorize = 'purple';
+            }
+        return this.colorize;
+        }
     }
 });
